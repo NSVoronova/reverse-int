cbcd01f3e76fefbceabf8f55b833bcfbff023fd7
Nadzeya Voranava <nadya.voronova@gmail.com>
2023-08-17 19:05:22 +0200

    init: add task reverse-Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let array =  Array.from(String(n));
+  if(n<0) {array.splice(0,1)};
+  let newArray = array.reverse(); 
+  newString = newArray.join("");
+  return +newString;
 }
